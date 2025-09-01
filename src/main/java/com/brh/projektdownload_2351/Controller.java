a8772a517ba0113115f675b11068b075b3f63fb6
srhrsb <sebastian.boettcher2@srh.de>
2025-09-01 14:45:23 +0200

    Stand nach 4UE

--- src/main/java/com/brh/projektdownload_2351/Controller.java
+++ src/main/java/com/brh/projektdownload_2351/Controller.java
@@ -3,6 +3,10 @@ package com.brh.projektdownload_2351;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
     @FXML
@@ -14,11 +18,22 @@ public class Controller {
 
     @FXML
     protected void onClickSearch() {
+        Stage currentStage = App.getStage();
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setInitialDirectory( new File( System.getProperty("user.home")+"/downloads"));
+        File selected = chooser.showDialog( currentStage );
 
+        if(selected != null){
+            String path = selected.getAbsolutePath();
+            targetTf.setText( path );
+        }
     }
 
     @FXML
     protected void onClickDownload() {
+          String path = urlTf.getText();
 
+          Download download = new Download(path, targetTf.getText());
+          download.execute();
     }
 }
---
+++ src/main/java/com/brh/projektdownload_2351/Download.java
@@ -0,0 +1,56 @@
+package com.brh.projektdownload_2351;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+public class Download{
+    private String link;
+    private String target;
+    private File outputFile;
+    public Download(String link, String target ) {
+        this.link = link;
+        this.target = target;
+
+    }
+    public void execute(){
+        try{
+            URL url = new URL(link);
+
+            //öffnen der Verbindung
+            HttpURLConnection connection =
+                    (HttpURLConnection) url.openConnection();
+
+            //Input Stream
+            BufferedInputStream buffInputStream =
+                    new BufferedInputStream( connection.getInputStream() );
+
+            //File Objekt um Dateinamen des Links zu ermitteln
+            File file =  new File(link);
+
+            //Ausgabefile mit dem Target-Pfad und übernommenem Dateinamen
+            outputFile = new File(target, file.getName());
+
+            //Outputstream mit Ziel
+            OutputStream outputStream = new FileOutputStream(outputFile);
+            BufferedOutputStream buffOutputStream =
+                    new BufferedOutputStream( outputStream , 1024);
+
+            byte[] buffer = new byte[1024];
+            int downloaded = 0;
+            int readByte = 0;
+
+            while((readByte = buffInputStream.read(buffer, 0, 1024)) >= 0){
+                buffOutputStream.write(buffer, 0, readByte);
+                downloaded += readByte;
+                System.out.println("Runtergeladen: "+downloaded);
+            }
+
+            buffOutputStream.close();
+            buffInputStream.close();
+            System.out.println("Download erfolgreich");
+        }
+        catch( IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
